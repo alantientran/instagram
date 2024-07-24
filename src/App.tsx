47a22f2a4d22e22b437d5f0c8fdfe0330ca7542f
alantientran <att0316698@gmail.com>
2024-07-24 18:54:40 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import AuthLayout from "./_auth/AuthLayout";
+import SigninForm from "./_auth/forms/SigninForm";
+import SignupForm from "./_auth/forms/SignupForm";
+import { Home } from "./_root/pages";
+import RootLayout from "./_root/RootLayout";
+import "./globals.css";
+import { Routes, Route } from "react-router-dom";
+
+const App = () => {
+  // h-screen tailwind shortcut to set height to vh-100 (viewport height)
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        {/* public routes */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+        </Route>
+        {/* private routes (after signing in) */}
+        // index route is the default route (starting page)
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  );
+};
+
+export default App;
---
+++ src/_auth/AuthLayout.tsx
@@ -0,0 +1,30 @@
+import { Outlet, Navigate } from "react-router-dom";
+
+const AuthLayout = () => {
+  const isAuthenticated = false;
+
+  return (
+    <>
+      {isAuthenticated ? (
+        // if user is authenticated, redirect to home page
+        <Navigate to="/" />
+      ) : (
+        <>
+          {/* justify-center centers horizontally and items-center centers 
+          vertically. flex-col makes components stack vertically. py-10 is 
+          'padding 10 units top and bottom*/}
+          <section className="flex flex-1 justify-center items-center flex-col py-10">
+            <Outlet />
+          </section>
+          <img
+            src="/assets/images/side-image.png"
+            alt="logo"
+            className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
+          />
+        </>
+      )}
+    </>
+  );
+};
+
+export default AuthLayout;
