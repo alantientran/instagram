2daa1413631300e2c948c3dd62a2a80099f3ec25
att0316698@gmail.com <att0316698@gmail.com>
2024-07-24 17:53:38 -0500

    assets not found

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { Button } from "@/components/ui/button";
 
 // using shadecn/ui is a UI library that uses tailwind for styling
 
 const SignupForm = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Button>click me</Button>
+    </div>
+  );
 };
 
 export default SignupForm;
