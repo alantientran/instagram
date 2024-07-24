50956f1f7cc0602be9b96f6dc32b7075947e9c5f
att0316698@gmail.com <att0316698@gmail.com>
2024-07-24 17:00:10 -0500

    shadecn setup

---
+++ src/_auth/forms/SignupForm.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+// using shadecn/ui is a UI library that uses tailwind for styling
+
+const SignupForm = () => {
+  return <div></div>;
+};
+
+export default SignupForm;
